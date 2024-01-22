715779e1b71b212edf4d1a8531911950189eff64
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-21 21:57:16 -0800

    Added comments

--- js/index.js
+++ js/index.js
@@ -8,28 +8,30 @@ document.addEventListener('DOMContentLoaded', function () {
     headerTitle.textContent = userMessages.headerTitle;
     header.appendChild(headerTitle);
 
-    // Links to Reader and Writer pages
+    // Making the nav and ul elements
     const nav = document.createElement('nav');
     const ul = document.createElement('ul');
 
+    // Writer link
     const writerLi = document.createElement('li');
-    const readerLi = document.createElement('li');
-
     const writerAnchor = document.createElement('a');
     writerAnchor.href = 'writer.html';
     writerAnchor.textContent = userMessages.writerHeader;
     writerLi.appendChild(writerAnchor);
     ul.appendChild(writerLi);
 
+    // Reader link
+    const readerLi = document.createElement('li');
     const readerAnchor = document.createElement('a');
     readerAnchor.href = 'reader.html';
     readerAnchor.textContent = userMessages.readerHeader;
     readerLi.appendChild(readerAnchor);
     ul.appendChild(readerLi);
 
+    // Append ul to nav
     nav.appendChild(ul);
 
-    // Append elements to body
+    // Append Header and Nav to body
     document.body.appendChild(header);
     document.body.appendChild(nav);
 });
