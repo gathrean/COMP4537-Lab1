43d80cce56848f8898d7a75c5644c2f1710865fd
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-21 23:41:57 -0800

    Added disclosure comments

--- js/reader.js
+++ js/reader.js
@@ -1,3 +1,11 @@
+// DISCLOSURE: I've used ChatGPT to get started with the following code, 
+// and I've fixed and tweaked it to make it work for this project along the way.
+
+// MY HOSTED WEBSITE: https://comp4537-lab1-gathrean.vercel.app/
+
+// In this lab I gained some experience with working with local storage and 
+// it kinda gave me some ideas on how chat messaging works (though it's more complex than this).
+
 const notesContainer = document.getElementById('notesDisplay');
 
 // Function to render notes to the DOM
--- js/writer.js
+++ js/writer.js
@@ -1,7 +1,10 @@
 // DISCLOSURE: I've used ChatGPT to get started with the following code, 
 // and I've fixed and tweaked it to make it work for this project along the way.
 
-// Along the way I've learned: 
+// MY HOSTED WEBSITE: https://comp4537-lab1-gathrean.vercel.app/
+
+// In this lab I gained some experience with working with local storage and 
+// it kinda gave me some ideas on how chat messaging works (though it's more complex than this).
 
 class Note {
     constructor(content) {
