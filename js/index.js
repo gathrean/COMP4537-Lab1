87ea34f98ad34ac201de98379c04929a697f61fc
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-21 21:55:52 -0800

    Added nav links to writer and reader pages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Title
     document.title = userMessages.headerTitle;
 
@@ -8,8 +8,28 @@ document.addEventListener('DOMContentLoaded', function() {
     headerTitle.textContent = userMessages.headerTitle;
     header.appendChild(headerTitle);
 
-    // Buttons
+    // Links to Reader and Writer pages
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+
+    const writerLi = document.createElement('li');
+    const readerLi = document.createElement('li');
+
+    const writerAnchor = document.createElement('a');
+    writerAnchor.href = 'writer.html';
+    writerAnchor.textContent = userMessages.writerHeader;
+    writerLi.appendChild(writerAnchor);
+    ul.appendChild(writerLi);
+
+    const readerAnchor = document.createElement('a');
+    readerAnchor.href = 'reader.html';
+    readerAnchor.textContent = userMessages.readerHeader;
+    readerLi.appendChild(readerAnchor);
+    ul.appendChild(readerLi);
+
+    nav.appendChild(ul);
 
     // Append elements to body
     document.body.appendChild(header);
+    document.body.appendChild(nav);
 });
--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,5 +1,7 @@
 const userMessages = {
     headerTitle: "Lab 1 - Gathrean Dela Cruz (A01167248)",
     writerButtonLabel: "Go to Writer",
-    readerButtonLabel: "Go to Reader"
+    writerHeader: "Writer",
+    readerButtonLabel: "Go to Reader",
+    readerHeader: "Reader",
 };
